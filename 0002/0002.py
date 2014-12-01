502ece26a1a769c505ada5385fa3f893209fd06c
MechanicLoyal <yzy.china@qq.com>
2014-12-01 05:52:47 -0800

    Add 0002.py

--- 0002/0002.py
+++ 0002/0002.py
@@ -2,10 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import mysql.connector
+import random
+
+def getcode(i):
+    code = ""
+    for t in range(i):
+        code = code + chr(random.randint(65, 90))
+    return code
+
+codelist = []
+for t in range(200):
+    newcode=getcode(24)
+    if newcode not in codelist:
+        codelist.append(getcode(24))
 
 conn = mysql.connector.connect(user='root', password='password', database='test', use_unicode=True)
 cursor = conn.cursor()
 
-cursor.execute('create table code(code varchar(24))')
+cursor.execute('create table code(id varchar(10) primary key, code varchar(24))')
 
-cursor.execute
+for t in range(len(codelist)):
+    cursor.execute('insert into code(id, code) values (%s, %s)', [t, codelist[t]])
+
+conn.commit()
+
+cursor.close()
+conn.close()
+
